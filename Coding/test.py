87d111773c974af463961ebbbd4d057cc26da27b
Vaseekaran <vvasee1996@gmail.com>
2020-02-15 23:17:31 +0530

    First Commit

---
+++ Coding/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import os
+
+os.chdir('C:/Users/Vaseekaran.AMIRTHAVARMAN/Desktop/Datastorm/Datasets')
+df = pd.read_csv('credit_card_default_train.csv')
+print(df.head())
+
+#admin06 - Arts Wifi Password
